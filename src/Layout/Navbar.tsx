41964b2f18cb211e8806b7c6a1795cc3a592d686
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-03-15 04:18:23 +0100

    jokes app from reddit

---
+++ src/Layout/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+    return (
+        <div className='w-full bg-slate-900 p-6 text-white font-semibold text-center'>Post Drafter</div>
+    )
+}
+
+export default Navbar
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,30 @@
 import React from "@astrojs/react"
+import { create } from "zustand"
 
 type Props = {}
 
+type Store = {
+    votes: number,
+    addVote: () => void,
+    subVote: () => void
+}
+
+
 const Page = (props: Props) => {
+    const useStore = create<Store>(set => ({
+        votes: 0,
+        addVote: () => set(state => ({votes: state.votes + 1})),
+        subVote: () => set(state => ({ votes: state.votes - 1}))
+    }))
+    
+    const getVotes = useStore(state => state.votes)
+    const addVotes = useStore(state => state.addVote)
+    console.log(getVotes)
     return (
-        <div>
+        <div className="font-semibold">
             Page 1
+            <h1>{getVotes} people have cast their votes</h1>
+            <button className='bg-red-400 p-2 rounded-md text-white' onClick={() => addVotes()}>Add Vote {getVotes}</button>
         </div>
     )
 }
