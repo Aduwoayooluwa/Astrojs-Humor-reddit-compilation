72e963fddcbc37afd8a0301a3282b71c725f33fa
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-03-15 03:40:44 +0100

    installing librarties

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from "@astrojs/vercel/serverless";
 
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [react()],
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
---
+++ src/components/Page.tsx
@@ -0,0 +1,13 @@
+import React from "@astrojs/react"
+
+type Props = {}
+
+const Page = (props: Props) => {
+    return (
+        <div>
+            Page 1
+        </div>
+    )
+}
+
+export default Page
