fe14b00fec99b7bb4af6169c5d81dd67ff8d44e0
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-03-15 05:17:08 +0100

    taking jokes from 9gag

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Navbar = (props: Props) => {
     return (
-        <div className='w-full bg-slate-900 p-6 text-white font-semibold text-center'>Post Drafter</div>
+        <div className='w-full bg-slate-900 p-6 text-white font-semibold text-center'>Humor Calls</div>
     )
 }
 
