ab1400978501dcc90a37e3ef18bdd49fa6234145
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-03-15 05:24:00 +0100

    path changing

--- src/hooks/useGetPosts.ts
+++ src/hooks/useGetPosts.ts
@@ -3,8 +3,7 @@ import useSWR from 'swr'
 
 export const getter = (url: string) => axios.get(url, {
     headers: {
-		'X-RapidAPI-Key': '9e89d4ea21msh48f2cd0ac7d903ep132579jsna3c3bc99b2a5',
-		'X-RapidAPI-Host': 'programmer-humor.p.rapidapi.com'
+		
 	}
 }
     )
